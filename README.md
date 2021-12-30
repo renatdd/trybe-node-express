c43408e0f372b0db82db4bf22b0e7b1693f2630f
Renato Feitosa <pythaqua@gmail.com>
2021-12-30 16:12:52 -0300

    Add project initial solution

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# trybe-node-express
-Projects developed during the Back End module of Trybe's Fullstack Web Development course.
+## Comandos utilizados
+    heroku create pythaqua-bd --remote heroku-bd  
+    git push heroku-bd pythaqua-sd-09-stranger-things-backend:master  
+    heroku create pythaqua-bk --remote heroku-bk  
+    git push heroku-bk pythaqua-sd-09-stranger-things-backend:master  
---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
+const StrangerThingsRepository = require('./data/repository/StrangerThings');
+const StrangerThingsService = require('./services/StrangerThings');
+
+const PORT = Number(process.env.PORT);
+const { UPSIDEDOWN_MODE } = process.env;
+
+const app = express();
+
+const strangerThingsRepository = new StrangerThingsRepository(
+  strangerThingsDataset,
+);
+const strangerThingsService = new StrangerThingsService(
+  strangerThingsRepository,
+);
+
+app.use(cors());
+
+const hereIsTheUpsideDown = JSON.parse(UPSIDEDOWN_MODE);
+
+app.get('/', (req, res) => {
+  const characters = strangerThingsService.search(
+    req.query,
+    hereIsTheUpsideDown,
+  );
+
+  res.status(200).json(characters);
+});
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
+});
